0d77713b5bcb8c0e2d2e016d2e45b72a9a1a12f5
David <www.975764577@qq.com>
2014-09-07 15:31:21 +0800

    Create FlattenBinaryTree.java

---
+++ FlattenBinaryTree.java
@@ -0,0 +1,64 @@
+//未能通过时间测试的代码，自己写的
+import java.util.ArrayDeque;
+public class Solution{
+	
+public static void flatten(TreeNode root)
+{	
+if(root==null||(root.left==null&&root.right==null)){
+return;		
+}
+ArrayDeque<TreeNode> firstStack=new  ArrayDeque<>();
+ArrayDeque<TreeNode> secondStack=new ArrayDeque<>();
+inOrder(root, firstStack, secondStack);
+System.out.println(secondStack.size());
+TreeNode head=root=secondStack.pollLast();  
+while(!secondStack.isEmpty()){	
+head.right=secondStack.pollLast();
+head=head.right;
+}
+while(root!=null){
+System.out.println(root.val);
+root=root.right;	
+}
+}
+public static void inOrder(TreeNode cur,ArrayDeque<TreeNode> firstStack,ArrayDeque<TreeNode> secondStack)
+{
+if(cur==null){	
+return;
+}
+firstStack.push(cur);
+while(!firstStack.isEmpty()){	
+TreeNode node=firstStack.pop();
+secondStack.push(node);
+if(node.right!=null){	
+firstStack.push(node.right);
+if(node.left!=null){	
+firstStack.push(node.left);
+}
+}	
+}
+}   
+public  static   void   main(String[]  args) 
+{	
+TreeNode  n1=new  TreeNode(1); 
+TreeNode  n2=new  TreeNode(2); 
+TreeNode  n3=new  TreeNode(3); 
+TreeNode  n4=new  TreeNode(4); 
+TreeNode  n5=new  TreeNode(5); 
+TreeNode  n6=new  TreeNode(6);
+n1.left=n2;
+n2.left=n3;
+n2.right=n4;
+n1.right=n5;
+n5.right=n6;
+flatten(null);
+}
+}
+class TreeNode
+{
+int  val; 	
+TreeNode left,right;
+public TreeNode(int  val){	
+this.val=val;	
+}
+}
