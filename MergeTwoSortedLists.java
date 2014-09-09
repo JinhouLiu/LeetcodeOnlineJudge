5061de270f5ed35732024161d53accc83d8ee190
David <www.975764577@qq.com>
2014-09-09 21:38:34 +0800

    Create MergeTwoSortedLists.java

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,31 @@
+public class Solution{
+
+//递归形式
+public ListNode mergeTwoLists(ListNode l1, ListNode l2)
+{
+if(l1==null){	
+return l2;
+}
+if(l2==null)
+{	
+return l1;
+}
+if (l1.val<l2.val){	
+l1.next=mergeTwoLists(l1.next, l2);
+return  l1;
+}else{	
+l2.next=mergeTwoLists(l1,l2.next);
+return  l2;
+}
+}
+public static void  main(String[] args)
+{
+//省略了一些测试代码
+}
+//非递归形式
+public ListNode mergeTwoLists(ListNode l1,ListNode l2)
+{
+
+
+}  
+}
