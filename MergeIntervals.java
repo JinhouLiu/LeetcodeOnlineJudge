4c1f3dd097e4fde51b80cf989d2398987ee5b47e
David <www.975764577@qq.com>
2014-10-06 14:21:34 +0800

    Update MergeIntervals.java

--- MergeIntervals.java
+++ MergeIntervals.java
@@ -18,7 +18,9 @@ for (int i = 0; i <list.size();i++)
 {
 System.out.println(list.get(i).start+"\t"+list.get(i).end);		
 }
-}  		
+}  
+
+//版本一　　当数据大时直接超时
 public static List<Interval> merge(List<Interval> intervals)
 {	
 List<Interval> list=new ArrayList<Interval>();
@@ -73,4 +75,54 @@ class Interval  implements Comparable<Interval>
         	return  0;
 		}
 	 }
+}
+
+//版本２　通过在线测试
+public static List<Interval> sortInterval(List<Interval> list){
+Collections.sort(list,new Comparator<Interval>(){
+@Override
+public int compare(Interval o1, Interval o2){
+if (o1.start>o2.start) {
+return 1;
+}else if (o1.start==o2.start){
+return 0;
+}else {
+return -1;
+}
+}
+});
+return   list;
+}
+public static List<Interval> merge(List<Interval> intervals)
+{	
+List<Interval> list=new ArrayList<Interval>();
+if(intervals==null||intervals.size()<2)
+{	
+return intervals; 	
+}
+intervals=sortInterval(intervals);
+Interval temp=intervals.get(0); 
+for(int i=1;i<intervals.size();i++)
+{
+if(temp.end>=intervals.get(i).start)
+{
+temp.end=Math.max(temp.end,intervals.get(i).end);	
+}
+else
+{	
+list.add(temp);	
+temp=intervals.get(i);
+}	
+}
+list.add(temp);
+return list; 
+}	
+}
+class Interval 
+{
+int start;
+int end;
+Interval() { start = 0; end = 0; }
+Interval(int s, int e) { start = s; end = e; }
+
 }
