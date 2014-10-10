33cfcbb1b615f4242294a0a41e4f421899bb5dad
David <www.975764577@qq.com>
2014-10-10 11:34:52 +0800

    Update GenerateParentheses.java

--- GenerateParentheses.java
+++ GenerateParentheses.java
@@ -31,3 +31,27 @@ result.addAll(generateParenthesis(n, left, right+1));
 return  result;   
 }             
 }
+//一种动态规划的方式
+import java.util.Arrays;
+import java.util.List;
+public class Solution
+{	
+public static List<String>  generateParenthesis(int n)
+{
+@SuppressWarnings("unchecked")
+List<String>[] result=new  List[n+1];
+result[0]=Arrays.asList("");
+for(int i = 1; i<=n;i++){
+   List<String> list=new   ArrayList<String>();   	
+	   for(int j = 0; j < i; j++){
+	        for(String  sl:result[j]) 
+		 	      for(String sr:result[i-j-1])
+				      list.add("("+sl+")"+sr);	
+	   }
+    result[i]=list; 	
+ }
+return  result[n]; 
+}
+  
+}
+
