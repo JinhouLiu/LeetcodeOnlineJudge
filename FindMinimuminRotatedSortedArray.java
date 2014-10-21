382f354a7db4ee3271defdc0e857e2b5ea15382f
David <www.975764577@qq.com>
2014-10-21 16:02:16 +0800

    Update FindMinimuminRotatedSortedArray.java

--- FindMinimuminRotatedSortedArray.java
+++ FindMinimuminRotatedSortedArray.java
@@ -29,29 +29,24 @@ public class Solution {
 
 对原题做的一个扩展，即数组中可以包含重复的数字
  public class Solution {
-	public static void main(String[] args) {
-		int[] num = { 1, 1, 1, 0, 1 };
-		System.out.println(findMin(num));
-	}
-
-	public static int findMin(int[] num) {
-		if (num == null || num.length == 0)
-			return 0;
-		int low = 0, high = num.length - 1;
+public static int findMin(int[] num) {
+		if (num == null || num.length == 0) return -1;
+		   					
+		int low = 0, high = num.length-1;
 		int mid = low;
-		while (num[low] >= num[high]) {
-
-			if (high - low == 1) {
+		while (num[low]>=num[high]) {
+			if (high -low== 1) {
 				mid = high;
 				break;
 			}
-			mid = low + (high - low) / 2;
-			if (num[mid] == num[high] && num[mid] == num[low]) {
-				return findInOrder(num, low, high);
+			mid = low + (high-low)/2;
+			if (num[mid] == num[high] && num[mid] == num[low])
+			{
+			return findInOrder(num, low, high);
 			}
 			if (num[mid] >= num[low]) {
 				low = mid;
-			} else if (num[mid] < num[high]) {
+			} else if (num[mid]<=num[high]) {
 				high = mid;
 			}
 		}
@@ -59,8 +54,8 @@ public class Solution {
 	}
 
 	public static int findInOrder(int[] num, int low, int high) {
-		int min = Integer.MAX_VALUE;
-		for (int i = low + 1; i < high; i++) {
+		int min = num[low];
+		for (int i = low + 1; i <=high; i++) {
 			if (num[i] < min) {
 				min = num[i];
 			}
